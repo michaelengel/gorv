a8be1c6e78764dd1a4fb02c763aae0e6810ce670
Your Name <you@example.com>
2019-10-12 21:20:52 +0200

    fix auipc

--- src/run.go
+++ src/run.go
@@ -9,198 +9,11 @@ import "strconv"
 var regs [32]uint32
 var pc   uint32
 
-// convert bits to 2's complement
-func to2(off uint32, bits int) int {
-	var off2 int
-	if (off >= (1 << bits)) { 
-		off  = off - (1<<(bits))
-		off  = ^off & ((1<<bits)-1)
-       		off2 = -int(off+1)
-	} else {
-		off2 = int(off)
-	}
-	return off2
-}
-
-// return register name for number
-func reg(nr uint32) string {
-	r := []string { "zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2", 
-			  "fp", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-			  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-			  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6" }
-	return r[int(nr)]
-}
-
-// join instruction components for disassembly
-func join(str ...string) string {
-	var joined string
-
-	for n, s := range str {
-		switch n {
-			case 0: joined = s
-			case 1:  joined = joined + " " + s
-			default: joined = joined + ", " + s
-		}
-	}
-	return joined
-}
-
-// disassemble given instruction
-func disasm(insn uint32) (string, byte) {
-	var mnem string
-
-	opcode := insn & 0b01111111
-
-	switch opcode { // p16 of RISC V book
-		case 0b0110111: 
-			mnem = "lui"
-			rd   := (insn & 0b00000000_00000000_00001111_10000000) >>  7
-			off  := (insn & 0b11111111_11111111_11110000_00000000) >> 12
-			dis  := join(mnem, reg(rd), strconv.Itoa(to2(off, 20)))
-			return dis, 'U'
-		case 0b0010111:
-			mnem = "auipc"
-			rd   := (insn & 0b00000000_00000000_00001111_10000000) >>  7
-			off  := (insn & 0b11111111_11111111_11110000_00000000) >> 12
-			dis  := join(mnem, reg(rd), strconv.Itoa(to2(off, 20)))
-			return dis, 'U'
-		case 0b1101111: 
-			mnem = "jal"
-			rd   := (insn & 0b00000000_00000000_00001111_10000000) >>  7
-			off  := (insn & 0b10000000_00000000_00000000_00000000) >> 31 << 20 | 
-			        (insn & 0b01111111_11100000_00000000_00000000) >> 21 <<  1 |
-			        (insn & 0b00000000_00010000_00000000_00000000) >> 20 << 11 |
-			        (insn & 0b00000000_00001111_11110000_00000000) >> 12 << 12 
-			return join(mnem, reg(rd), strconv.Itoa(int(off))), 'J'
-		case 0b1100111: 
-			mnem = "jalr"
-			rd   := (insn & 0b00000000_00000000_00001111_10000000) >>  7
-			rs1  := (insn & 0b00000000_00001111_10000000_00000000) >> 15
-			off  := (insn & 0b11111111_11110000_00000000_00000000) >> 20
-			return join(mnem, reg(rd), reg(rs1), strconv.Itoa(int(off))), 'I'
-		case 0b1100011: 
-			cond := (insn & 0b00000000_00000000_01110000_00000000) >> 12
-			rs1  := (insn & 0b00000000_00001111_10000000_00000000) >> 15
-			rs2  := (insn & 0b00000001_11110000_00000000_00000000) >> 20
-			off  := (insn & 0b10000000_00000000_00000000_00000000) >> 31 << 12 | 
-			        (insn & 0b01111110_00000000_00000000_00000000) >> 25 <<  5 |
-			        (insn & 0b00000000_00000000_00001111_00000000) >>  8 <<  1 |
-			        (insn & 0b00000000_00000000_00000000_10000000) >>  7 << 11 
-			switch cond {
-				case 0b000: mnem = "beq"
-				case 0b001: mnem = "bne"
-				case 0b100: mnem = "blt"
-				case 0b101: mnem = "bge"
-				case 0b110: mnem = "bltu"
-				case 0b111: mnem = "bgtu"
-				default:    mnem = "b??"
-			}
-			return join(mnem, reg(rs1), reg(rs2), strconv.Itoa(to2(off, 12))), 'B'
-		case 0b0000011:
-			size := (insn & 0b00000000_00000000_01110000_00000000) >> 12
-			rd   := (insn & 0b00000000_00000000_00001111_10000000) >>  7
-			rs1  := (insn & 0b00000000_00001111_10000000_00000000) >> 15
-			off  := (insn & 0b11111111_11110000_00000000_00000000) >> 20
-
-			switch size {
-				case 0b000: mnem = "lb"
-				case 0b001: mnem = "lh"
-				case 0b010: mnem = "lw"
-				case 0b100: mnem = "lbu"
-				case 0b101: mnem = "lhu"
-				default:    mnem = "l??"
-			}
-                        dis  := join(mnem, reg(rd), strconv.Itoa(to2(off, 11)), "("+reg(rs1)+")")
-                        return dis, 'I'
-		case 0b0100011:
-			size := (insn & 0b00000000_00000000_01110000_00000000) >> 12
-			rs1  := (insn & 0b00000000_00001111_10000000_00000000) >> 15
-			rs2  := (insn & 0b00000001_11110000_00000000_00000000) >> 20
-			off  := (insn & 0b11111110_00000000_00000000_00000000) >> 25 << 5 | 
-			        (insn & 0b00000000_00000000_00001111_10000000) >>  7
-          
-			switch size {
-				case 0b000: mnem = "sb"
-				case 0b001: mnem = "sh"
-				case 0b010: mnem = "sw"
-				default:    mnem = "l??"
-			}
-                        dis  := join(mnem, reg(rs2), strconv.Itoa(to2(off, 11)), "("+reg(rs1)+")")
-                        return dis, 'S'
-		case 0b0010011: 
-			var off2 int
-
-			aluop :=(insn & 0b00000000_00000000_01110000_00000000) >> 12
-			rd   := (insn & 0b00000000_00000000_00001111_10000000) >>  7
-			rs1  := (insn & 0b00000000_00001111_10000000_00000000) >> 15
-			off  := (insn & 0b11111111_11110000_00000000_00000000) >> 20
-			if (off >= 0b10000000_0000) { 
-				off  = off - (1<<11)
-				off  = ^off & 0b011111111111
-                                off2 = -int(off+1)
-			} else {
-				off2 = int(off)
-			}
-			switch aluop {
-				case 0b000: mnem = "addi"
-				case 0b010: mnem = "slti"
-				case 0b011: mnem = "sltiu"
-				case 0b100: mnem = "xori"
-				case 0b110: mnem = "ori"
-				case 0b111: mnem = "andi"
-				case 0b001: mnem = "slli"
-				case 0b101: mnem = "srli" // or srai...
-				default:    mnem = "alu??i"
-			}
-                        dis  := join(mnem, reg(rd), reg(rs1), strconv.Itoa(off2))
-                        return dis, 'I'
-		case 0b0110011: 
-			aluop  := (insn & 0b00000000_00000000_01110000_00000000) >> 12
-			aluop2 := (insn & 0b11111110_00000000_00000000_00000000) >> 25
-			rd   :=   (insn & 0b00000000_00000000_00001111_10000000) >>  7
-			rs1  :=   (insn & 0b00000000_00001111_10000000_00000000) >> 15
-			rs2  :=   (insn & 0b00000001_11110000_00000000_00000000) >> 20
-			switch aluop {
-				case 0b000: 
-					mnem = "alu??"
-					if (aluop2 == 0b0000000) { mnem = "add" }
-					if (aluop2 == 0b0100000) { mnem = "sub" }
-				case 0b001: mnem = "sll"
-				case 0b010: mnem = "slt"
-				case 0b011: mnem = "sltu"
-				case 0b100: mnem = "xor"
-				case 0b101:
-					mnem = "alu??"
-					if (aluop2 == 0b0000000) { mnem = "srl" }
-					if (aluop2 == 0b0100000) { mnem = "sra" }
-				case 0b110: mnem = "or"
-				case 0b111: mnem = "and"
-				default:    mnem = "alu??"
-			}
-                        dis  := join(mnem, reg(rd), reg(rs1), reg(rs2))
-                        return dis, 'R'
-		case 0b1110011: return "sys", 'I'
-		default:        return "unknown", '?'
-	}
-}
-
-func printregs() {
-	fmt.Printf("PC = %08x\n", pc)
-	for n, s := range regs {
-		fmt.Printf("%s = %08x", reg(uint32(n)), s)
-		if (n%8 == 7) {
-			fmt.Printf("\n")
-		} else {
-			fmt.Printf(" ")
-		}
-	}
-}
-
 // emulate given instruction
 func emulate(insn uint32) {
 	var mnem string
 
-	opcode := insn & 0b01111111
+	opcode := insn & 0b00000000_00000000_00000000_01111111
 
 	switch opcode { // p16 of RISC V book
 		case 0b0110111: 
@@ -214,7 +27,8 @@ func emulate(insn uint32) {
 			mnem = "auipc"
 			rd   := (insn & 0b00000000_00000000_00001111_10000000) >>  7
 			off  := (insn & 0b11111111_11111111_11110000_00000000) >> 12
-			pc = regs[rd] + uint32(int(pc) + to2(off, 20))
+                        off = off << 12
+			regs[rd] = uint32(int(pc) + to2(off, 20))
 
 		case 0b1101111: 
 			// TODO: signeness, *2???
@@ -260,7 +74,7 @@ func emulate(insn uint32) {
 			} else {
 				pc = pc + 4
 			}
-			fmt.Printf("****** new pc = %08x off = %08x off = %d\n", pc, off, to2(off, 12))
+			fmt.Printf("****** new pc = %08x off = %08x = %d\n", pc, off, to2(off, 12))
 
 		case 0b0000011:
 			// TODO: transfer sizes, signedness!
